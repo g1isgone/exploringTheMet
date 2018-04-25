4a8ea31cb090977bc60c3dd269eb2623e4db3f02
g1isgone <jchung@smith.edu>
2018-04-25 14:16:29 -0400

    randpop exp

---
+++ experimentRandPopToFreq.m
@@ -0,0 +1,31 @@
+load('RandPop_Step5.mat');
+%%
+randPopFreq50 = pathToFreqMat(Randwalk50);
+randPopFreq51 = pathToFreqMat(Randwalk51);
+randPopFreq52 = pathToFreqMat(Randwalk52);
+randPopFreq53 = pathToFreqMat(Randwalk53);
+randPopFreq54 = pathToFreqMat(Randwalk54);
+randPopFreq55 = pathToFreqMat(Randwalk55);
+randPopFreq56 = pathToFreqMat(Randwalk56);
+randPopFreq57 = pathToFreqMat(Randwalk57);
+randPopFreq58 = pathToFreqMat(Randwalk58);
+randPopFreq59 = pathToFreqMat(Randwalk59);
+
+randPop5FreqAvg = (randPopFreq50 + randPopFreq51 + randPopFreq52 + randPopFreq53 +randPopFreq54 + randPopFreq55 + randPopFreq56 + randPopFreq57 + randPopFreq58 + randPopFreq59)/10; 
+ 
+
+%%
+randPopFreq100 = pathToFreqMat(Randwalk100);
+randPopFreq101 = pathToFreqMat(Randwalk101);
+randPopFreq102 = pathToFreqMat(Randwalk102);
+randPopFreq103 = pathToFreqMat(Randwalk103);
+randPopFreq104 = pathToFreqMat(Randwalk104);
+randPopFreq105 = pathToFreqMat(Randwalk105);
+randPopFreq106 = pathToFreqMat(Randwalk106);
+randPopFreq107 = pathToFreqMat(Randwalk107);
+randPopFreq108 = pathToFreqMat(Randwalk108);
+randPopFreq109 = pathToFreqMat(Randwalk109);
+
+randPop10FreqAvg = (randPopFreq100 + randPopFreq101 + randPopFreq102 + randPopFreq103 +randPopFreq104 + randPopFreq105 + randPopFreq106 + randPopFreq107 + randPopFreq108 + randPopFreq109)/10; 
+ 
+
