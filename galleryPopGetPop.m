025d71f7fa4e854d5b837a89260203b23dd7f0c6
g1isgone <jchung@smith.edu>
2018-04-25 10:45:36 -0400

    popular gets more popular

--- galleryPopGetPop.m
+++ galleryPopGetPop.m
@@ -1,27 +1,26 @@
-%Given the popularity of each node, the number of steps, and the start
-%location --> Figure out the path the viewer will most likely go to
-%depending on the popularity of the painting. It will also return the
-%probability to go from one gallery to another. 
-function [visitedGalleries, visitedProb] = galleryProbWalk(popGraph, numSteps, startLocation)
+function [visitedGalleries, visitedProb] = galleryPopGetPop(popGraph, startLocation, numSteps)
     visitedGalleries = zeros(1, numSteps); %initialization of data structure that denotes which galleries you have been to
     visitedProb = zeros(1, numSteps); %initialization of data structure that denotes the probability to go from one gallery to another
     
     curr = startLocation; %the current location we are on 
     cumProb = 1; %the cumulative probability
-     
-    for stepI = 1:numSteps %for each number of steps
+    
+    for stepI = 1:numSteps 
         popAllNeighbors = popGraph(curr,:); %extract all the popularity from that node
         nbrs = find(popAllNeighbors > 0 ); %find the nbr nodes' ids
         popNbrs = popAllNeighbors(nbrs); %extract the popularity of all the nbrs
+   
+        popNbrNbrs = popGraph(nbrs, :); %extract the popularity for the nbrs' neighbors
+        localPop = sum(popNbrNbrs, 2)' + popNbrs; %the local popularity for a neighbor (includes nbr + its nbrs popularity)
         
-        totalPop = sum(popNbrs); %the sum of all the popularity of the neighbors
+        totalPop = sum(localPop); %the sum of all the popularity of the localPopularity
         randPop = randi(totalPop); %chooses a random popularity between 1 - totalPop
         
         nbrIndex = 1; %nbrIndex of nbrs - not the actual nbr node id  
         
         %find the node that with the range that includes randPop
         while(randPop > 0 )  %as long as randPop is greater than 0 
-            randPop = randPop-popNbrs(nbrIndex); %subtract population range of a nbr 
+            randPop = randPop-localPop(nbrIndex); %subtract population range of a nbr 
             nbrIndex = nbrIndex+1;  %update nbrIndex
         end 
         
@@ -30,12 +29,11 @@ function [visitedGalleries, visitedProb] = galleryProbWalk(popGraph, numSteps, s
         visitedGalleries(stepI) = curr; %store the nbr we are traversing to 
         
         
-        %calculating probability to go to that gallery 
-        nbrProb = popNbrs/totalPop; %the probability of all the nbrs 
-        cumProb = cumProb*nbrProb(nbrIndex); %the cumulative probability to go from the previous path of galleries to this new gallery
+        %calculating probability to go to that nbr based on the local prob
+        localProb = localPop/totalPop; %the local probability of all the nbrs
+        cumProb = cumProb*localProb(nbrIndex); %the cumulative probability to go from the previous path of galleries to this new gallery
         visitedProb(stepI) = cumProb; %store this cumulative probability 
-   
-    end 
-    visitedGalleries = [startLocation visitedGalleries]; %store the list of visitedGalleries
-    visitedProb = [1 visitedProb]; %store the cumulative probability to go from one gallery to the next  
+
+    end
+        
 end
