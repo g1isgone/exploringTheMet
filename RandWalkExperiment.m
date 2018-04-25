cd3c6fcec14b09a7dfb516e1b1fe010b47ce143a
czang97 <crystalzang@Crystals-MacBook-Air.local>
2018-04-25 00:58:27 -0400

    matrix

--- RandWalkExperiment.m
+++ RandWalkExperiment.m
@@ -1,3 +1,4 @@
+%%Random Walk with step 5
 m51=randomWalk(A, 5);
 m52=randomWalk(A, 5);
 m53=randomWalk(A, 5);
@@ -10,6 +11,18 @@ m59=randomWalk(A, 5);
 m510=randomWalk(A, 5);
 save randomWalk5 m51 m52 m53 m54 m55 m56 m57 m58 m59 m59 m510
 
+%Random Walk with step 10
+m101=randomWalk(A, 10);
+m102=randomWalk(A, 10);
+m103=randomWalk(A, 10);
+m104=randomWalk(A, 10);
+m105=randomWalk(A, 10);
+m106=randomWalk(A, 10);
+m107=randomWalk(A, 10);
+m108=randomWalk(A, 10);
+m109=randomWalk(A, 10);
+m1010=randomWalk(A, 10);
+save randomWalk10 m101 m102 m103 m104 m105 m106 m107 m108 m109 m1010 
 
 %Random Walk for 20 steps
 m201=randomWalk(A, 20);
@@ -24,4 +37,17 @@ m209=randomWalk(A, 20);
 m210=randomWalk(A, 20);
 save randomWalk20 m201 m202 m203 m204 m205 m206 m207 m208 m209 m210
 
-%5, 10, 20, 40, 80
+%Random Walk for 40 steps
+m401=randomWalk(A, 40);
+m402=randomWalk(A, 40);
+m403=randomWalk(A, 40);
+m404=randomWalk(A, 40);
+m405=randomWalk(A, 40);
+m406=randomWalk(A, 40);
+m407=randomWalk(A, 40);
+m408=randomWalk(A, 40);
+m409=randomWalk(A, 40);
+m410=randomWalk(A, 40);
+save randomWalk40 m401 m402 m403 m404 m405 m406 m407 m408 m409 m410
+
+%80
