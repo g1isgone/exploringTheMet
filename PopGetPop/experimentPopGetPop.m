37590fab6f097ce4133b7b30197e7919791a4f89
czang97 <crystalzang@Crystals-MacBook-Air.local>
2018-04-26 08:18:09 -0400

    PopGetPop.CZ

---
+++ PopGetPop/experimentPopGetPop.m
@@ -0,0 +1,8 @@
+Randwalk50=[];
+RandwalkProb50 = [];
+for i=1:45
+    [Randwalk, RandwalkProb] =galleryPopGetPop(popMat, 5, i);
+    Randwalk50=[Randwalk50; Randwalk];
+    RandwalkProb50 = [RandwalkProb50; RandwalkProb]; 
+end
+ 
