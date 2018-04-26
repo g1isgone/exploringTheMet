0ee6cb8f2c85f97b1c5c768ea1ae19aba94c0aee
TYQ <TYQ@appledeMacBook-Pro.local>
2018-04-25 20:04:31 -0400

    Figures

---
+++ RandPop/experimentRandPopToFreq.m
@@ -0,0 +1,73 @@
+
+%%
+randPopFreq50 = pathToFreqMat(Randwalk50);
+randPopFreq51 = pathToFreqMat(Randwalk51);
+randPopFreq52 = pathToFreqMat(Randwalk52);
+randPopFreq53 = pathToFreqMat(Randwalk53);
+randPopFreq54 = pathToFreqMat(Randwalk54);
+randPopFreq55 = pathToFreqMat(Randwalk55);
+randPopFreq56 = pathToFreqMat(Randwalk56);
+randPopFreq57 = pathToFreqMat(Randwalk57);
+randPopFreq58 = pathToFreqMat(Randwalk58);
+randPopFreq59 = pathToFreqMat(Randwalk59);
+
+randPop5FreqAvg = (randPopFreq50 + randPopFreq51 + randPopFreq52 + randPopFreq53 +randPopFreq54 + randPopFreq55 + randPopFreq56 + randPopFreq57 + randPopFreq58 + randPopFreq59)/10; 
+ 
+
+%%
+randPopFreq100 = pathToFreqMat(Randwalk100);
+randPopFreq101 = pathToFreqMat(Randwalk101);
+randPopFreq102 = pathToFreqMat(Randwalk102);
+randPopFreq103 = pathToFreqMat(Randwalk103);
+randPopFreq104 = pathToFreqMat(Randwalk104);
+randPopFreq105 = pathToFreqMat(Randwalk105);
+randPopFreq106 = pathToFreqMat(Randwalk106);
+randPopFreq107 = pathToFreqMat(Randwalk107);
+randPopFreq108 = pathToFreqMat(Randwalk108);
+randPopFreq109 = pathToFreqMat(Randwalk109);
+
+randPop10FreqAvg = (randPopFreq100 + randPopFreq101 + randPopFreq102 + randPopFreq103 +randPopFreq104 + randPopFreq105 + randPopFreq106 + randPopFreq107 + randPopFreq108 + randPopFreq109)/10; 
+
+%%
+randPopFreq200 = pathToFreqMat(Randwalk200);
+randPopFreq201 = pathToFreqMat(Randwalk201);
+randPopFreq202 = pathToFreqMat(Randwalk202);
+randPopFreq203 = pathToFreqMat(Randwalk203);
+randPopFreq204 = pathToFreqMat(Randwalk204);
+randPopFreq205 = pathToFreqMat(Randwalk205);
+randPopFreq206 = pathToFreqMat(Randwalk206);
+randPopFreq207 = pathToFreqMat(Randwalk207);
+randPopFreq208 = pathToFreqMat(Randwalk208);
+randPopFreq209 = pathToFreqMat(Randwalk209);
+
+randPop20FreqAvg = (randPopFreq200 + randPopFreq201 + randPopFreq202 + randPopFreq203 +randPopFreq204 + randPopFreq205 + randPopFreq206 + randPopFreq207 + randPopFreq208 + randPopFreq209)/10; 
+%%
+randPopFreq400 = pathToFreqMat(Randwalk400);
+randPopFreq401 = pathToFreqMat(Randwalk401);
+randPopFreq402 = pathToFreqMat(Randwalk402);
+randPopFreq403 = pathToFreqMat(Randwalk403);
+randPopFreq404 = pathToFreqMat(Randwalk404);
+randPopFreq405 = pathToFreqMat(Randwalk405);
+randPopFreq406 = pathToFreqMat(Randwalk406);
+randPopFreq407 = pathToFreqMat(Randwalk407);
+randPopFreq408 = pathToFreqMat(Randwalk408);
+randPopFreq409 = pathToFreqMat(Randwalk409);
+
+randPop40FreqAvg = (randPopFreq400 + randPopFreq401 + randPopFreq402 + randPopFreq403 +randPopFreq404 + randPopFreq405 + randPopFreq406 + randPopFreq407 + randPopFreq408 + randPopFreq409)/10;
+%%
+randPopFreq800 = pathToFreqMat(Randwalk800);
+randPopFreq801 = pathToFreqMat(Randwalk801);
+randPopFreq802 = pathToFreqMat(Randwalk802);
+randPopFreq803 = pathToFreqMat(Randwalk803);
+randPopFreq804 = pathToFreqMat(Randwalk804);
+randPopFreq805 = pathToFreqMat(Randwalk805);
+randPopFreq806 = pathToFreqMat(Randwalk806);
+randPopFreq807 = pathToFreqMat(Randwalk807);
+randPopFreq808 = pathToFreqMat(Randwalk808);
+randPopFreq809 = pathToFreqMat(Randwalk809);
+
+randPop80FreqAvg = (randPopFreq800 + randPopFreq801 + randPopFreq802 + randPopFreq803 +randPopFreq804 + randPopFreq805 + randPopFreq806 + randPopFreq807 + randPopFreq808 + randPopFreq809)/10;
+
+ 
+ 
+
